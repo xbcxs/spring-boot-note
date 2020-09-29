51b43c7a779c2a3e64a852a0d02eaddc95c85cc1
xbcxs <xbcxs@163.com>
2020-09-29 19:07:31 +0800

    完成spring-boot-note-flyway

--- spring-boot-note-flyway/src/main/java/com/xbcxs/flyway/FlywayApplication.java
+++ spring-boot-note-flyway/src/main/java/com/xbcxs/flyway/FlywayApplication.java
@@ -3,7 +3,6 @@ package com.xbcxs.flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class FlywayApplication {
 
