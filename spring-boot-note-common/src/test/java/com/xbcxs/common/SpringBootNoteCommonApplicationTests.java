bed5373ca1397f42b4987a608dd15bc65b04980f
xbcxs <xbcxs@163.com>
2020-10-14 18:50:10 +0800

    新增：新增common和quartz待完成模块

---
+++ spring-boot-note-common/src/test/java/com/xbcxs/common/SpringBootNoteCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbcxs.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNoteCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
