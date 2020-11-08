1b2780b06299acea651d57d9b06075e3cc15fcc7
xbcxs <xbcxs@163.com>
2020-11-09 00:10:13 +0800

    fix: 修复代理MAP无法PUT问题

--- spring-boot-note-common/src/main/java/com/xbcxs/common/form/DataFormatter.java
+++ spring-boot-note-common/src/main/java/com/xbcxs/common/form/DataFormatter.java
@@ -6,66 +6,37 @@ import java.util.List;
 import java.util.Map;
 
 /**
- *
  * @author X
  */
 public class DataFormatter {
 
-    public static void main(String[] args) {
-        // 数据库查询所有节点
-        List<Map> originalList = null;
-        String topNodeId = null;
-        test(topNodeId, originalList);
+    public static List<Map> format(String parentId, List<Map> list) {
+        List topList = new ArrayList();
+        Map top = new HashMap(4);
+        top.put("id", parentId);
+        top.put("children", null);
+        topList.add(top);
+        Map map = (Map) buildTree(topList, list).get(0);
+        return (List<Map>) map.get("children");
     }
 
-    /**
-     * 将有结构的List数据构建成JSON树结构数据
-     *    {
-     *        "id": "",
-     *        "name": "",
-     *        "children": [
-     *            {
-     *            "id": "",
-     *            "name": "",
-     *            "children": []
-     *            },
-     *        ]
-     *    }
-     * @param topNodeId
-     * @param originalList
-     * @return
-     */
-    public static List<Map> test(String topNodeId, List<Map> originalList) {
-        // 构建虚拟顶层节点
-        List<Map> topList = new ArrayList<>();
-        Map topMap = new HashMap();
-        topMap.put("id", topNodeId);
-        topList.add(topMap);
-        formListToTree(topList, originalList);
-        return (List<Map>)topMap.get("children");
-    }
-
-    /**
-     * 含结构信息的List格式化成有层级结构的List
-     *
-     * @param parentList
-     * @param originalList
-     */
-    public static void formListToTree(List<Map> parentList, List<Map> originalList) {
-        for (Map parentMap : parentList) {
-            // 广度遍历获取该节点所有一级子节点
-            String parentId = String.valueOf(parentMap.get("id"));
-            List<Map> childrenList = new ArrayList<>();
-            for(Map originalMap : originalList){
-                if(parentId.equals(String.valueOf(originalMap.get("parentId")))){
-                    childrenList.add(originalMap);
+    public static List<Map> buildTree(List<Map> parentList, List<Map> originalList) {
+        for (Map parent : parentList) {
+            List childrenList = new ArrayList<>();
+            for (Map current : originalList) {
+                if (parent.get("id").equals(current.get("parentId"))) {
+                    Map newMap = new HashMap();
+                    // 将originalList中代理MAP转换成标准MAP防止删除失败。
+                    new HashMap().putAll(current);
+                    childrenList.add(newMap);
                 }
             }
-            if(!childrenList.isEmpty()){
-                parentMap.put("children", childrenList);
+            if (!childrenList.isEmpty()) {
+                parent.put("children", childrenList);
                 originalList.removeAll(childrenList);
-                formListToTree(childrenList, childrenList);
+                buildTree(childrenList, originalList);
             }
         }
+        return parentList;
     }
 }
